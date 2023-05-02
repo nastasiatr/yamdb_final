f9451ecfb5cd26069465126d1a35093a94aed7c2
Анастасия Трусова <trusovaanastasia@mail.ru>
2023-05-02 11:54:03 +0800

    2 этап

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -9,7 +9,7 @@ SECRET_KEY = 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['51.250.16.171']
 
 # Application definition
 
